11ad5a1cb2a5dc96e13a986aef22c901dd378e5e
Patrick Vasconcelos <89patrick89@gmail.com>
2022-08-23 17:53:23 -0300

    update crawlers

--- python_scripts/crawlers.py
+++ python_scripts/crawlers.py
@@ -6,12 +6,30 @@ from time import sleep
 import requests
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from abc import abstractmethod, ABC
+from abc import abstractmethod
+from credentials import username, password
+
 
 
 class Crawler(ABC):
 
-    def __init__(self) -> None:
-        super().__init__()
+    s = Service('C:/Users/Qorpo/.wdm/drivers/chromedriver/win32/chromedriver.exe')
+    options = Options()
+    options.add_argument('window-size=400,800')
 
-    
+    def __init__(self, url) -> None:
+        super().__init__()
+        self.url = url
+        
+
+    def _sendCredentials(self) -> None:
+        self.driver.find_element(By.NAME, 'login').send_keys(username)
+        self.driver.find_element(By.NAME, 'senha').send_keys(password)
+
+    @abstractmethod
+    def get_data(self, **kwargs):
+        self.driver = webdriver.Chrome(service=self.s,options=self.options)
+        self.driver.get(self.url)
+        self._sendCredentials
+        pass
+
--- python_scripts/main.py
+++ python_scripts/main.py
@@ -2,9 +2,13 @@ from datetime import timedelta,date
 import datetime
 from ingestors import AgendamentoIngestor
 from writers import DataWriter
+from crawlers import Crawler
 
 
 
 
-agendamento_ingestor = AgendamentoIngestor(writer=DataWriter,startDate=date.today() - timedelta(days=1), endDate=date.today() - timedelta(days=1))
-agendamento_ingestor.ingest()
+# agendamento_ingestor = AgendamentoIngestor(writer=DataWriter,startDate=date.today() - timedelta(days=1), endDate=date.today() - timedelta(days=1))
+# agendamento_ingestor.ingest()
+
+
+crawler = Crawler('https://clinicweb.vitta.me/clinicweb/login.jsp')
