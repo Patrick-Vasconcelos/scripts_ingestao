775edcfbbc0230e7f5bbcb6275057343b41e1cd7
Patrick Vasconcelos <89patrick89@gmail.com>
2022-08-29 17:51:33 -0300

    updating CrawlerConsulta

--- python_scripts/crawlers.py
+++ python_scripts/crawlers.py
@@ -1,10 +1,12 @@
 from abc import ABC
-from cgitb import text
+from datetime import date, timedelta
+from operator import attrgetter
+import pandas as pd
+import datetime
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs
 from selenium.webdriver.chrome.options import Options
 import time
-import requests
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from abc import abstractmethod
@@ -21,10 +23,18 @@ class Crawler(ABC):
         super().__init__()
         self.url = 'https://clinicweb.vitta.me/clinicweb/login.jsp'
     
-    def clicarXpath(self, xpath: str) -> None:
-        self.driver.find_element(By.XPATH, xpath).click()
-        print(f"Clicando no botão: {self.driver.find_element(By.XPATH, xpath).text}\n")
+    def timer(self) -> None:
         time.sleep(3)
+    
+    
+    def clicarXpath(self, xpath: str, msg : str = None) -> None:
+
+        if msg is None:
+            print(f"Clicando no botão: {self.driver.find_element(By.XPATH, xpath).text}")
+        else: 
+            print(msg)
+        self.driver.find_element(By.XPATH, xpath).click()
+        self.timer()
         
 
     def _sendCredentials(self) -> None:
@@ -40,15 +50,71 @@ class Crawler(ABC):
 
 class CrawlerConsulta(Crawler):
 
+
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
+
+    def sendDates(self,startDate : datetime.date, endDate: datetime.date) -> None:
+
+        self.startDate = startDate.strftime('%d/%m/%Y')
+        self.endDate = startDate.strftime('%d/%m/%Y')
+        print(f"Consultando relatorio de consultas de {startDate} ate {endDate}")
+
+        start_date = self.driver.find_element(By.NAME, 'de')
+        start_date.clear()
+        start_date.send_keys(self.startDate)
+
+        end_date = self.driver.find_element(By.NAME, 'ate')
+        end_date.clear()
+        end_date.send_keys(self.endDate)
+
+
     
-    def scrape(self, **kwargs) -> None:
+    def scrape(self, startDate : datetime.date, endDate : datetime.date, **kwargs) -> None:
         self.driver = webdriver.Chrome(service=self.s)
         self.driver.maximize_window()
         self.driver.get(self.url)
         self._sendCredentials()
 
+        self.timer()
+        
         self.clicarXpath('//*[@id="tab_top"]/li[4]/a') # clicando em financeiro
         self.clicarXpath('//*[@id="tab_top"]/li[4]/ul/li[2]/a') # clicando em relatorio
         self.clicarXpath('//*[@id="tab_top"]/li[4]/ul/li[2]/ul/li[10]/a') # clicando em financeiro
+        self.sendDates(startDate=startDate,endDate=endDate) # atribuindo data de inicio e de fim
+        self.clicarXpath(xpath='//*[@id="frmcadastrar"]/div[1]/div[26]/div[2]/label', msg='Retirando a opção SADT')
+        self.clicarXpath(xpath='//*[@id="frmcadastrar"]/div[1]/div[27]/div/div[2]/label', msg='Selecionando para não separar guias')
+        self.clicarXpath(xpath='//*[@id="containerFooter"]/div/div/a/span', msg='Clicando em gerar relatório')
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        
+        self.page_content = self.driver.page_source
+        
+
+        dados_consultas = []
+        site = bs(self.page_content, 'html.parser')
+        
+        
+
+        site = site.find('table')     
+        consultas = site.find_all('tr')
+
+        for consulta in consultas:
+            rows = consulta.find_all('td')
+            for row in rows:
+                print(row.text)
+
+        """
+        df = pd.DataFrame(columns= ['Empresa', 'Data de Registro' , 'Hora de Registro',
+                            'Data', 'Hora', 'Paciente', 'Carteirinha', 'CPF do Paciente',
+                            'Dmed gerado', 'Pagador', 'Executante', 'Solicitante', 'Qtde',
+                            'Procedimento', 'Grupo de Procedimento', 'Especialidade', 'Convênio',
+                            'Plano', 'Lote', 'Pagamento', 'N. Guia', 'Taxa do Cartão (R$(%))',
+                            'Recebido Bruto(R$)', 'Imposto(R$)', 'Recebido Líquido(R$)',
+                            'Tipo de Pagamento', 'Forma de Pagamento', 'Nota Fiscal', 'Valor(R$)',
+                            'Observação (Faturamento)', 'Observação (Pagamento)'])
+        for row in consulta.tbody.find_all('tr'):
+            columns = row.find_all('td')
+
+            if(columns != []):
+
+        """
--- python_scripts/main.py
+++ python_scripts/main.py
@@ -11,10 +11,12 @@ from crawlers import Crawler
 # agendamento_ingestor = AgendamentoIngestor(writer=DataWriter,startDate=date.today() - timedelta(days=1), endDate=date.today() - timedelta(days=1))
 # agendamento_ingestor.ingest()
 
-agendamento_ingestor = AgendamentoIngestor(writer=DataWriter,startDate=date.today() - timedelta(days=1), endDate=date.today())
-agendamento_ingestor.ingest()
+# agendamento_ingestor = AgendamentoIngestor(writer=DataWriter,startDate=date.today() - timedelta(days=1), endDate=date.today())
+# agendamento_ingestor.ingest()
 
 
-# crawler = CrawlerConsulta()
+crawler = CrawlerConsulta()
 
-# crawler.scrape()
+# crawler.scrape(startDate=date.today() - timedelta(days=1),endDate=date.today() - timedelta(days=1))
+crawler.scrape(startDate=datetime.date(2022, 8, 29),endDate=datetime.date(2022, 8, 29))
+
