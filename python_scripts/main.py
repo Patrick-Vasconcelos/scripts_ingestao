1f03c94be315662bf4ad97f1138b8d620b49d75f
Patrick Vasconcelos <89patrick89@gmail.com>
2022-09-02 09:14:42 -0300

    update main

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -4,18 +4,19 @@ from datetime import timedelta,date
 import datetime
 from ingestors import AgendamentoIngestor
 from crawlers import CrawlerConsulta
+from writers import S3Writer
 from writers import DataWriter
 from crawlers import Crawler
 
 
-# agendamento_ingestor = AgendamentoIngestor(writer=DataWriter,startDate=date.today() - timedelta(days=1), endDate=date.today() - timedelta(days=1))
-# agendamento_ingestor.ingest()
+agendamento_ingestor = AgendamentoIngestor(writer=S3Writer,startDate=date.today() - timedelta(days=1), endDate=date.today() - timedelta(days=1))
+agendamento_ingestor.ingest()
 
 # agendamento_ingestor = AgendamentoIngestor(writer=DataWriter,startDate=date.today() - timedelta(days=1), endDate=date.today())
 # agendamento_ingestor.ingest()
 
 
-crawler = CrawlerConsulta()
-Date = datetime.date(2022, 8, 30)
+# crawler = CrawlerConsulta()
+# Date = datetime.date(2022, 8, 30)
 # crawler.scrape(startDate=date.today() - timedelta(days=1),endDate=date.today() - timedelta(days=1))
-crawler.scrape(startDate=Date,endDate=Date)
+# crawler.scrape(startDate=Date,endDate=Date)
